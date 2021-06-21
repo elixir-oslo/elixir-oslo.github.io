4970b1096ff641fc297065ef835a594efc34c353
Sveinung Gundersen <sveinugu@gmail.com>
2021-06-21 13:07:58 +0200

    Made build work from a Conda environment

--- .build.sh
+++ .build.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 set -ex
-source /usr/local/rvm/environments/ruby-2.4.1
+#source /usr/local/rvm/environments/ruby-2.4.1
 
 ruby --version
-rvm --version
+#rvm --version
 gem --version
 
-gem install bundler
+gem install bundler:1.15.4
 
+bundle config build.nokogiri --use-system-libraries
 bundle install
 # Check internal links
-bundle exec htmlproofer ./_site --check-html --allow-hash-href --assume-extension --disable-external
+#bundle exec htmlproofer ./_site --check-html --allow-hash-href --assume-extension --disable-external
 # Check external
 #bundle exec htmlproofer ./_site --check-html --allow-hash-href --assume-extension --external-only
-bundle exec jekyll build
+#bundle exec jekyll build
+make clean
+make build
+make check_http_urls
+
